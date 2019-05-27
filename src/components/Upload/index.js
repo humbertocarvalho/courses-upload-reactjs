232c55d46471fffe11a086f74873049528756a3e
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-05-26 21:22:57 -0300

    Created Upload component with DropZone.

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import Dropzone from 'react-dropzone';
+
+import { DropContainer, UploadMessage } from './styles';
 
 export default class Upload extends Component {
   render() {
-    return <div />;
+    return (
+      <Dropzone accept='image/*' onDropAccepted={() => {}}>
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps} />
+          </DropContainer>
+        )}
+      </Dropzone>
+    );
   }
 }
