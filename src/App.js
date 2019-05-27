764a3bdbdf6ea0c9370875b24999276fb08a7708
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-05-26 21:05:35 -0300

    Created Container/Content styles.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import GlobalStyle from './styles/global';
+import { Container, Content } from './styles';
 
 class App extends Component {
   render() {
-    return <GlobalStyle />;
+    return (
+      <Container>
+        <Content>Teste</Content>
+        <GlobalStyle />
+      </Container>
+    );
   }
 }
 export default App;
